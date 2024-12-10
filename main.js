1071518ebee9984516e177d95813b7b73d1afc4f
JulianaSalcedo <julianasalcedo13@gmail.com>
2024-12-10 10:13:19 -0500

    primera entrega

---
+++ main.js
@@ -0,0 +1,34 @@
+
+alert ("VAMOS A JUGAR")
+
+let pregunta1 = prompt("¿Cuándo acabó la II Guerra Mundial? 1 - 1945  2 - 1932  3 - 1665")
+
+if (pregunta1 == 1){
+  alert("CORRECTO")
+}else{
+  alert ("INCORRECTO")
+}
+
+
+let pregunta2 = prompt("¿Qué es JavaScript? 1 - Un lenguaje de programación  2 - Un editor de código")
+
+if (pregunta2 == 1){
+  alert("CORRECTO")
+}else{
+  alert ("INCORRECTO")
+}
+
+
+let pregunta3 = prompt("¿En qué país se encuentra la torre de Pisa? 1 - Paris  2 - Italia  3 - Inglaterra")
+
+if (pregunta3 == 2){
+  alert("CORRECTO")
+}else{
+  alert ("INCORRECTO")
+}
+
+
+
+
+
+
